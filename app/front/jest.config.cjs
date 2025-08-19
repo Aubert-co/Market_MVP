27eca86c261cc7cbe4fa2252b2ac7aa172133f7d
aubert-co <blacket021@gmail.com>
2025-08-19 19:56:15 -0300

    chore: adicionar dependencias para test

---
+++ app/front/jest.config.cjs
@@ -0,0 +1,14 @@
+// jest.config.cjs
+/** @type {import('jest').Config} */
+module.exports = {
+  preset:'ts-jest',
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+  "moduleNameMapper": {
+    "\\.(jpg|jpeg|png|gif|svg)$": "jest-transform-stub"
+  },
+
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
+};
