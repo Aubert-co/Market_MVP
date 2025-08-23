1518a20191665cbcc90774b0469218058e618344
aubert-co <blacket021@gmail.com>
2025-08-22 23:56:11 -0300

    chore: message component no longer uses context

--- app/front/src/components/boxMessages.tsx
+++ app/front/src/components/boxMessages.tsx
@@ -1,16 +1,34 @@
-import { useMessage } from "../context/messageContext";
+import { useEffect, useState } from "react";
+
+export type Message ={
+    content:string,
+    type:'error' | 'info' | 'success'
+}
 
 
-export const BoxMessage = ()=>{
-   const {message}= useMessage()
-    return (
-        <>
-            {message?.content && (
-                <div className={"message_"+message.type} >
-                    <p data-testid="message_content">{message.content}</p>
-                </div>
-            )}
-        </>
-    );
+export const useBoxMessage = ()=>{
+    const [message,setMessage] = useState<Message>({
+        content:'',type:'info'
+    })
+    useEffect(()=>{
+        if(message.content){
+            setTimeout(()=>{
+                setMessage({content:'',type:'info'})
+            },3000)
+        }
+    },[message])
+    const BoxMessage = ()=>{
+   
+        return (
+            <>
+                {message?.content && (
+                    <div className={"message_"+message.type} >
+                        <p data-testid="message_content">{message.content}</p>
+                    </div>
+                )}
+            </>
+        );
     
-};
+    };
+    return {message,setMessage,BoxMessage}
+}
