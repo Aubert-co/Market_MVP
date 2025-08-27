daa7488bdbbf0b93d8cd4655e848da64d668d8b2
aubert-co <blacket021@gmail.com>
2025-08-27 17:36:04 -0300

    feat: add Header component folder

---
+++ app/front/src/components/header/seachBar.tsx
@@ -0,0 +1,29 @@
+import { useRef } from "react"
+import { useNavigate } from "react-router-dom"
+import { getInputValue } from "../../utils"
+
+export const SearchBar = ()=>{
+    const navigate = useNavigate()
+    const searchRef = useRef<HTMLInputElement >(null)
+
+    const onClick = (e:React.FormEvent<HTMLFormElement>)=>{
+        e.preventDefault()
+        let search = getInputValue(searchRef)
+
+        if( !search ){
+            return;
+        }
+      
+        navigate(`/buscas/${search}`)
+        
+    }
+    return (
+
+        <form onSubmit={(e)=>onClick(e)} className="search">
+            <div className="search-items">
+                <input ref={searchRef}  name="input_search"  minLength={2} maxLength={20} className="input_search"  placeholder="FAÇA UMA BUSCA"/>
+                <button className="btn_search" type="submit" >BUSCAR</button>
+            </div>
+        </form>
+    )
+}
---
+++ app/front/src/components/header/topBar.tsx
@@ -0,0 +1,24 @@
+import { Link, useNavigate } from "react-router-dom"
+import { SearchBar } from "./seachBar"
+import { FaShoppingCart, FaUser } from "react-icons/fa"
+
+export const TopBar = ()=>{
+    const navigate = useNavigate()
+    return(
+        <>
+            <div className="logo">
+                <Link to={"/"}>SUPERSTORE</Link>
+            </div>
+            <SearchBar/>
+            <nav>
+                <i>
+                    <FaShoppingCart onClick={()=>navigate("/perfil/carrinho")}/>
+                </i>
+
+                <i>
+                  <FaUser onClick={()=>navigate("/perfil/ordens")}/>
+                </i>
+            </nav>
+        </>
+    )
+}
