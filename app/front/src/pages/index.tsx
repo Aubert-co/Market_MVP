9fe1954cc7efd62a945831ca8c101e9d8760fe4f
aubert-co <blacket021@gmail.com>
2025-08-27 17:41:45 -0300

    feat: add initial products page

---
+++ app/front/src/pages/index.tsx
@@ -0,0 +1,35 @@
+import { usePagination } from "../components/pagination"
+import { useNavigate } from "react-router-dom"
+import { Container } from "../components/layoutContainer"
+import { BoxProducts } from "../components/product/boxProducts"
+import type { Product } from "../types/products.types"
+import { useState,useEffect } from "react"
+import { serviceGetProducts } from "../services/productsService"
+import { fetchProducts } from "../services/fetchDatas"
+
+type ProductState ={
+  datas: Product[];
+  status: number;
+}
+export const Index = ()=>{
+    const navigate = useNavigate()
+    const changePage = (page:number)=>{
+        navigate(`/products/pages?value=${page}`)
+    }
+    const {setPagesInfos,pageInfos,Pagination} = usePagination(changePage)
+    const [products,setProducts] = useState<ProductState>({
+        datas:[],
+        status:0
+    })
+    useEffect(() => {
+        fetchProducts({setPages:setPagesInfos,setProducts,service:serviceGetProducts,pages:pageInfos.currentPage})
+        
+        setPagesInfos({currentPage:0,totalPages:0})
+    }, [pageInfos.currentPage,setPagesInfos]);
+    return (
+        <Container>
+            <BoxProducts datas={products.datas} status={products.status}/>
+            <Pagination/>
+        </Container>
+    )
+}
