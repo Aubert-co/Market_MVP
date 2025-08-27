1ace0abf236213c3a678d99499aa8bfe25a4d72b
aubert-co <blacket021@gmail.com>
2025-08-27 17:37:12 -0300

    feat: layoutContainer

---
+++ app/front/src/components/layoutContainer.tsx
@@ -0,0 +1,21 @@
+import {  ContainerStyle, Header, Main } from "../styles/index"
+import { TopBar } from "./header/topBar"
+import type React from "react"
+
+type Props={
+    children:React.ReactNode
+}
+export const Container = ({children}:Props)=>{
+    return(
+    <ContainerStyle>
+        <Header>
+            <TopBar/>
+        </Header>
+     
+        <Main>
+            {children}
+        </Main>
+        
+    </ContainerStyle>
+    )
+}
