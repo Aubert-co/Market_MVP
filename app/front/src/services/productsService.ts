5154dec3b995497f1e76a4ab3cd8f72fd6352c33
aubert-co <blacket021@gmail.com>
2025-08-27 17:46:38 -0300

    feat(services): add product service to fetch data

---
+++ app/front/src/services/productsService.ts
@@ -0,0 +1,20 @@
+import type { Product } from "../types/products.types";
+import { api } from "../constants/urls";
+type GetProducts = {
+    datas:Product[],
+    status:number,
+    currentPage:number,
+    totalPages:number
+}
+export const serviceGetProducts = async(pageNumber:number):Promise<GetProducts>=>{
+    try{
+        const response = await fetch(api+`/product/page/${pageNumber}`,{
+            credentials:'include'
+        })
+        if(!response.ok)throw new Error();
+        const {datas,currentPage,totalPages} = await response.json()
+        return {datas,currentPage,totalPages,status:response.status}
+    }catch(err:unknown){
+        throw new Error();
+    }
+}
