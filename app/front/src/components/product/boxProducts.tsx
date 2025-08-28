a6945a84a297bca2670cf7130f2229ce451ef7d5
aubert-co <blacket021@gmail.com>
2025-08-28 18:37:51 -0300

    refactor: add loading and refactor error

--- app/front/src/components/product/boxProducts.tsx
+++ app/front/src/components/product/boxProducts.tsx
@@ -2,23 +2,53 @@ import { ListProducts } from "./listProducts"
 import type { Product } from "../../types/products.types"
 import { ProductSection } from "../../styles/index"
 import { ErrorBox } from "../errorBox"
+import { ProductSkeleton } from "./boxProductSkeleton"
+import { useEffect, useState } from "react"
 
 type Props = {
     datas:Product[],
     status:number
 }
 
-export const BoxProducts = ({datas,status}:Props)=>{
-    if(status > 203 || datas.length === 0 ){
-        return <ErrorBox retry={true}/>
-    }
+type BoxState = "loading" | "error" | "alert" | "success"
+
+export const BoxProducts = ({ datas, status }: Props) => {
+    const [boxState, setBoxState] = useState<BoxState>("loading")
+
+    useEffect(() => {
+        if (status === 0) {
+            setBoxState("loading")
+            return
+        }
+
+        if (status > 201) {
+            setBoxState("error")
+            return
+        }
+
+        if (status === 201 && datas.length === 0) {
+            setBoxState("alert")
+            return
+        }
+
+        setBoxState("success")
+    }, [status, datas])
+
+    if (boxState === "error") return <ErrorBox retry />;
+    
+
+    if (boxState === "alert") return <ErrorBox message="Sem produtos disponíveis" />;
+    
 
     return (
         <ProductSection>
-            <div className="product-container">
-                <ListProducts listType={'Product'} products={datas}  />
-            </div>
+        <div className="product-container">
+            {boxState === "loading" ? 
+            <ProductSkeleton length={8} className="product" />
+            : 
+            <ListProducts listType="Product" products={datas} />
+            }
+        </div>
         </ProductSection>
-        
     )
 }
