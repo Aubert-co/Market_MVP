aedc682ca8753160a576e82e1476a9de1806ba84
aubert-co <blacket021@gmail.com>
2025-08-31 02:01:12 -0300

    test(unit): add tests for boxProductDetail, collapse, listComments and rating

---
+++ app/front/src/test/unit/boxProductDetail.test.tsx
@@ -0,0 +1,47 @@
+import { BoxProductDetail } from "@/components/product/boxProductDetail"
+import { ListProductDetail } from "@/components/product/listProductDetail"
+import { render, screen } from "@testing-library/react"
+
+describe("Component ListProductDetail",()=>{
+    const setMessage = jest.fn()
+    it("should render 'Produto não encontrado' when product is empty and status is valid",()=>{
+        const {queryByTestId,queryByText} = render(
+            <BoxProductDetail 
+            datas={{
+                product:[],
+                ratings:{_avg:{rating:0},_count:{rating:0}},
+                reviews:[],
+                comments:[]
+            }}
+            status={200} 
+            setMessage={setMessage}/>
+        )
+        expect(queryByText("Produto não encontrado")).toBeInTheDocument()
+        expect(queryByTestId("comments")).not.toBeInTheDocument()
+        expect(queryByTestId("product-detail")).not.toBeInTheDocument()
+        expect(queryByText("Ocorreu um erro ao carregar os dados.")).not.toBeInTheDocument()
+    })
+     it("should render 'Ocorreu um erro ao carregar os dados' when product is empty and status is greater than 201",()=>{
+        const {queryByTestId,queryByText} = render(
+            <BoxProductDetail datas={
+            {product:[],ratings:{_avg:{rating:0},_count:{rating:0}},reviews:[],comments:[]}}
+            status={202} setMessage={setMessage}/>
+        )
+        expect(queryByText("Produto não encontrado")).not.toBeInTheDocument()
+        expect(queryByText("Ocorreu um erro ao carregar os dados.")).toBeInTheDocument()
+        expect(queryByTestId("comments")).not.toBeInTheDocument()
+        expect(queryByTestId("product-detail")).not.toBeInTheDocument()
+    })
+    it("should render 'skeleton' when status is equal 0",()=>{
+        const {queryByTestId,queryByText} = render(
+            <BoxProductDetail datas={
+            {product:[],ratings:{_avg:{rating:0},_count:{rating:0}},reviews:[],comments:[]}}
+            status={0} setMessage={setMessage}/>
+        )
+        expect(queryByTestId("skeleton")).toBeInTheDocument()
+        expect(queryByText("Produto não encontrado")).not.toBeInTheDocument()
+        expect(queryByText("Ocorreu um erro ao carregar os dados.")).not.toBeInTheDocument()
+        expect(queryByTestId("comments")).not.toBeInTheDocument()
+        expect(queryByTestId("product-detail")).not.toBeInTheDocument()
+    })
+})
