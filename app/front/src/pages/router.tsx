807a6f3f0f9aa1dc82588ee410ad6d792bf48e98
aubert-co <blacket021@gmail.com>
2025-09-01 00:31:13 -0300

    chore: add profile page route

--- app/front/src/pages/router.tsx
+++ app/front/src/pages/router.tsx
@@ -4,18 +4,19 @@ import { Register } from "./register"
 import { Login } from "./login"
 import { Index } from "."
 import { ProductDetail } from "./productDetail"
+import { Profile } from "./profile"
 
 
 
 export const App = ()=>{
     return(
-   
     <Router>
         <Routes>
             <Route>
                 <Route path="/registro" element={<Register/>}/>
                 <Route path="/login" element={<Login/>}/>
                 <Route path="/" element={<Index/>}/>
+                <Route path="/perfil/:action" element={<Profile/>}/>
                 <Route path="/produto/:productid" element={<ProductDetail/>}/>
             </Route>
         </Routes>
