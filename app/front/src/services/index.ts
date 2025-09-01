58465a59647521f8a50b11ebe08d5594e0995e6d
aubert-co <blacket021@gmail.com>
2025-09-01 00:32:02 -0300

    chore: add usablefetch

--- app/front/src/services/index.ts
+++ app/front/src/services/index.ts
@@ -4,3 +4,20 @@ export const  headers ={'Content-Type':'application/json'}
 
 
 export const loadImage = (imageName:string)=>api+`/images/${imageName}`
+export type Response ={
+    message:string,
+    status:number
+}
+
+type UsableFetch<T> = {
+    setDatas:(args:{datas:T,status:number})=>void,
+    service:()=>Promise<{datas:T,status:number}>,
+}
+export const usableFetch =async <T> ({setDatas,service}:UsableFetch<T>)=>{
+    try{
+        const {datas,status} = await service()
+        setDatas({datas,status})
+    }catch(err:any){
+        setDatas({datas:[] as any,status:0})
+    }
+}
