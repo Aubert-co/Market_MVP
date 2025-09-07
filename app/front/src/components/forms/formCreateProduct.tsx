739b9687aa1d39909d140c797cabd364d38f58c7
aubert-co <blacket021@gmail.com>
2025-09-07 00:18:05 -0300

    feat: add form create product

---
+++ app/front/src/components/forms/formCreateProduct.tsx
@@ -0,0 +1,126 @@
+import { useRef } from "react"
+import { categories } from "@/constants"
+import { getMultiInputValues } from "@/utils"
+import { checkIsAValidCategory, checkIsAValidNumber, getValidImageFile, isAValidString } from "@/utils/checkIsValid"
+import { StyleBtn, UserFormStyles } from "@/styles/forms"
+import { InputWithLabel } from "./inputWithLabel"
+
+import { serviceCreateProduct } from "@/services/admStore.services"
+import { useBoxMessage } from "../boxMessages"
+
+type Props = {
+    storeId:string
+}
+export const FormCreateProduct = ({storeId}:Props)=>{
+    const nameRef = useRef<HTMLInputElement>(null)
+    const descriptionRef = useRef<HTMLTextAreaElement>(null)
+    const imageRef = useRef<HTMLInputElement>(null);
+    const priceRef = useRef<HTMLInputElement>(null)
+    const stockRef = useRef<HTMLInputElement>(null)
+    const categoryRef = useRef<HTMLSelectElement>(null)
+    const {setMessage,BoxMessage} = useBoxMessage()
+    const submit = async(e:React.FormEvent<HTMLFormElement>)=>{
+        e.preventDefault()
+        const [name,description,price,stock,category] = getMultiInputValues(
+            nameRef,descriptionRef,priceRef,stockRef,categoryRef
+        );
+        if(!isAValidString(name)){
+            setMessage({content:'Digite um nome válido',type:'info'})
+            return;
+        }
+        if(!isAValidString(description,199)){
+            setMessage({content:'Digite uma descrição válida',type:'info'})
+            return;
+        }
+        if(!checkIsAValidNumber(price)){
+            setMessage({content:'Digite um preço válido, apenas números',type:'info'})
+            return;
+        }
+        if(!checkIsAValidNumber(stock)){
+            setMessage({content:'Digite um estoque válido , apenas números',type:'info'})
+            return
+        }
+        if(!checkIsAValidCategory(category)){
+            setMessage({content:'Selecione uma cátegoria',type:'info'})
+            return
+        }
+        const file = getValidImageFile(imageRef);
+               
+        if(!file){
+            setMessage({content:'Adicione uma imagem válida',type:'info'})
+            return
+        }
+        const {message,status} = await serviceCreateProduct({name,description,price,stock,category,storeId,image:file})
+        
+        if(status === 201){
+            setMessage({content:'Produto criado com sucesso',type:'success'})
+        }   
+        setMessage({content:message,type:'info'})
+        
+    }
+    return(
+        <UserFormStyles>
+            <form onSubmit={submit}>
+                <h1 className="type_form">Criar produto</h1>
+                <BoxMessage/>
+                <InputWithLabel inputName="product_name" textLabel="Defina um nome que ajude os clientes a encontrarem seu produto">
+                    <input placeholder="Ex: camisa polo" className="input-form"
+                    ref={nameRef} 
+                    id="product_name"
+                    maxLength={15} 
+                    type="text" />
+                </InputWithLabel>
+
+                <InputWithLabel inputName="image" textLabel="Imagem do produto:">
+                    <input className="input-form"
+                        ref={imageRef} 
+                        type="file" 
+                        id="image" 
+                        accept="image/*"
+                        />
+                </InputWithLabel>
+
+                <InputWithLabel inputName="description" textLabel="Descrição do produto" >
+                     <textarea placeholder="Ex: uma camisa para eventos..."
+                        className="input-form" 
+                        ref={descriptionRef} 
+                        id="description" 
+                        maxLength={199}
+                        />
+                </InputWithLabel>
+
+                <InputWithLabel textLabel="Digite o preço do seu produto" inputName="price">
+                    <input 
+                    placeholder="Ex: 19.99"
+                    className="input-form" 
+                    ref={priceRef} 
+                    type="number" 
+                    id="price"/>
+                </InputWithLabel>
+
+                <InputWithLabel textLabel="Digite a quantidade de produtos" inputName="stock">
+                    <input 
+                    placeholder="Ex: 10" 
+                    className="input-form"
+                    ref={stockRef} 
+                    type="number" 
+                    id="stock"/>
+                </InputWithLabel>
+                
+                <InputWithLabel textLabel="Selecione a categoria que mais representa o seu produto" inputName="category">
+                    <select ref={categoryRef} id="category" >
+                    <option value="">Selecione uma categoria</option>
+                    {categories.map((category) => (
+                        <option key={category} value={category}>
+                        {category}
+                        </option>
+                    ))}
+                </select>
+                </InputWithLabel>
+                  <BoxMessage/>
+                <StyleBtn >Enviar</StyleBtn>
+              
+            </form>
+        </UserFormStyles>
+    )    
+}
