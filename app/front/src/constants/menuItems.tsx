86f5281d8b0b443f931ef4f005ce124da63a1a7a
aubert-co <blacket021@gmail.com>
2025-09-09 23:22:37 -0300

    feat: add menu items to sidebar

---
+++ app/front/src/constants/menuItems.tsx
@@ -0,0 +1,23 @@
+import { FaBox, FaClipboardList, FaTags, FaUsers, FaHome, FaSignOutAlt, FaCog } from "react-icons/fa"
+
+export const sideBarMenuItems = [
+  { label: "Dashboard", icon: <FaBox />, isActive: false, linkTo: "/loja" },
+  { label: "Produtos", icon: <FaClipboardList />, isActive: true, linkTo: "/loja/produtos" },
+  { label: "Cupons", icon: <FaTags />, isActive: false, linkTo: "/loja/cupons" },
+  { label: "Pedidos", icon: <FaUsers />, isActive: false, linkTo: "/loja/pedidos" },
+  { label: "Configurações",icon:<FaCog/>, isActive: false, linkTo: "/loja/configs" },
+
+  { label: "Voltar pro início", icon: <FaHome />, isActive: false, linkTo: "/" },
+  { label: "Sair", icon: <FaSignOutAlt />, isActive: false, linkTo: "/logout" },
+]
+
+type Label = "Dashboard" | "Produtos" | "Cupons" | "Pedidos" | "Configurações"
+export const selectMenuItem = (label:Label)=>{
+    return sideBarMenuItems.map((val)=>{
+        val.isActive = false
+        if(val.label === label){
+            val.isActive = true
+        }
+        return val
+    })
+}
