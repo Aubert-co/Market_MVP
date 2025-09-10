70831686b31279224869e31d4f0e109627a7eaf2
aubert-co <blacket021@gmail.com>
2025-09-09 23:46:39 -0300

    chore: remove a backend API URL

--- app/front/src/constants/urls.ts
+++ app/front/src/constants/urls.ts
@@ -1,4 +1,2 @@
-export const api = "http://localhost:8080"
-
-export const loginApi = `${api}/login`
-export const registerApi = `${api}/register`
+export const loginApi = `/login`
+export const registerApi = `/register`
