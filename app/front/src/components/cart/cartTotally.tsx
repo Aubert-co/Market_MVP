017d86c0deb585ff06504ffcdd2f02d1d6ea8550
aubert-co <blacket021@gmail.com>
2025-09-10 00:00:53 -0300

    chore: remove unused items

--- app/front/src/components/cart/cartTotally.tsx
+++ app/front/src/components/cart/cartTotally.tsx
@@ -1,6 +1,6 @@
 import type {UpdateCartState} from "@/context/cart.context"
 import { getItemsFromCart } from "@/storage/cart.storage"
-import { SmallButton, StyleBtn } from "@/styles/forms"
+import { SmallButton } from "@/styles/forms"
 import { useEffect ,useState} from "react"
 import styled from "styled-components"
 
