40192e393d302b24479524c1144a2538325e4109
aubert-co <blacket021@gmail.com>
2025-09-16 02:30:07 -0300

    chore: update the type UpdateCartState

--- app/front/src/context/cart.context.tsx
+++ app/front/src/context/cart.context.tsx
@@ -3,7 +3,7 @@ import { createContext } from "react"
 export type UpdateCart = boolean
 export type UpdateCartState = {
     updateCart:UpdateCart,
-    setUpdateCart:(state:UpdateCart)=>void,
+    setUpdateCart: React.Dispatch<React.SetStateAction<UpdateCart>>
 }
 
 export const UpdateCartContext = createContext<UpdateCartState | undefined>(undefined)
