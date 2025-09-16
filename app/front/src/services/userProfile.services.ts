e12f951500a0697a98023ea3e557daaa11aa1648
aubert-co <blacket021@gmail.com>
2025-09-16 02:11:05 -0300

    feat: add services for user profile

---
+++ app/front/src/services/userProfile.services.ts
@@ -0,0 +1,25 @@
+import type { BaseCoupon } from "@/types/coupons.types"
+import type { ResponseDatas } from "@/types/services.types"
+
+
+
+export const userCoupons = async():Promise<ResponseDatas<BaseCoupon<number>[]>>=>{
+    try{
+        const response = await fetch('/user/list/coupons',{
+            method:'GET',
+            credentials:'include',
+            headers:{
+                'Content-type':'json'
+            }
+        })
+        if(!response.ok)throw new Error();
+        const {datas} = await response.json()
+
+        return {datas,message:'Success',status:response.status}
+        
+    }catch(err:unknown){
+        return {status:500,message:'Algo deu errado',datas:[] as BaseCoupon<number>[]}
+    }
+
+
+}
