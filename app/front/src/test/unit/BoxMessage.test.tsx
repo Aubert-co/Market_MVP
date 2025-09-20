0e083d15c4514978774c5508bbf6154697630af0
aubert-co <blacket021@gmail.com>
2025-09-20 17:16:13 -0300

    chore: add toast handling to BoxMessage test

--- app/front/src/test/unit/BoxMessage.test.tsx
+++ app/front/src/test/unit/BoxMessage.test.tsx
@@ -13,7 +13,7 @@ describe("BoxMessage",()=>{
   
     it("should render for 3 seconds by default when no duration is provided",async()=>{
         const TestComponent= () => {
-            const { setMessage,BoxMessage } = useBoxMessage()
+            const { setMessage,BoxMessage } = useBoxMessage({styledType:'toast'})
             const onClick = ()=>{
                 setMessage({content:'lorem ipstu',type:'info'})
             }
@@ -25,14 +25,15 @@ describe("BoxMessage",()=>{
                
             )
         }
-        const {getByText,queryByText,container} = render(
+        const {getByText,queryByText,queryByTestId} = render(
             <TestComponent/>
         )
         const button = getByText('Click');
         const text = 'lorem ipstu'
         fireEvent.click(button)
         expect(queryByText(text)).toBeVisible()
-        expect(container.querySelector('div')).toHaveClass('message_info')
+      
+        expect(queryByTestId("message_")).toHaveClass('message_info')
         await waitFor(()=>{
             expect(queryByText(text)).toBeVisible()
         },{timeout:2999})
@@ -42,7 +43,7 @@ describe("BoxMessage",()=>{
     })
    it("should render the div with type 'error' correctly",async()=>{
         const TestComponent= () => {
-            const { setMessage,BoxMessage } = useBoxMessage()
+            const { setMessage,BoxMessage } = useBoxMessage({styledType:''})
             const onClick = ()=>{
                 setMessage({content:'lorem ipstu',type:'error'})
             }
@@ -54,14 +55,14 @@ describe("BoxMessage",()=>{
                
             )
         }
-        const {getByText,queryByText,container} = render(
+        const {getByText,queryByText,queryByTestId} = render(
             <TestComponent/>
         )
         const button = getByText('Click');
         const text = 'lorem ipstu'
         fireEvent.click(button)
         expect(queryByText(text)).toBeVisible()
-        expect(container.querySelector('div')).toHaveClass('message_error')
+        expect(queryByTestId("message_")).toHaveClass('message_error')
         await waitFor(()=>{
             expect(queryByText(text)).toBeVisible()
         },{timeout:2999})
@@ -71,7 +72,7 @@ describe("BoxMessage",()=>{
     })
     it("should render the div with type 'success' correctly",async()=>{
         const TestComponent= () => {
-            const { setMessage,BoxMessage } = useBoxMessage()
+            const { setMessage,BoxMessage } = useBoxMessage({styledType:''})
             const onClick = ()=>{
                 setMessage({content:'lorem ipstu',type:'success'})
             }
@@ -83,14 +84,14 @@ describe("BoxMessage",()=>{
                
             )
         }
-        const {getByText,queryByText,container} = render(
+        const {getByText,queryByText,queryByTestId} = render(
             <TestComponent/>
         )
         const button = getByText('Click');
         const text = 'lorem ipstu'
         fireEvent.click(button)
         expect(queryByText(text)).toBeVisible()
-        expect(container.querySelector('div')).toHaveClass('message_success')
+        expect(queryByTestId("message_")).toHaveClass('message_success')
         await waitFor(()=>{
             expect(queryByText(text)).toBeVisible()
         },{timeout:2999})
