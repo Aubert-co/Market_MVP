f5d8497e7bb1afc1744c37b454b96acd221f5e54
aubert-co <blacket021@gmail.com>
2025-09-22 14:35:11 -0300

    chore: update useCart fixtures

--- app/front/src/test/fixtures/index.ts
+++ app/front/src/test/fixtures/index.ts
@@ -47,51 +47,60 @@ export const userCartMocks: UserCart[] = [
   {
     id:1,
     productId: 1,
-    imageUrl: "https://via.placeholder.com/150",
+    product:{
+      imageUrl: "https://via.placeholder.com/150",
+      stock: 10,
+      name: "Fone Bluetooth",
+      price: 49.99,
+    },
     quantity: 2,
-    storeId: 101,
-    price: 49.99,
-    stock: 10,
-    name: "Fone Bluetooth"
   },
   {
     id:2,
     productId: 2,
-    imageUrl: "https://via.placeholder.com/150",
+    product:{
+      imageUrl: "https://via.placeholder.com/150",
+      name: "Mouse Gamer RGB",
+      price: 89.9,
+      stock: 5,
+    },
     quantity: 1,
-    storeId: 102,
-    price: 89.9,
-    stock: 5,
-    name: "Mouse Gamer RGB"
   },
   {
    id:3,
     productId: 3,
-    imageUrl: "https://via.placeholder.com/150",
+    product:{
+      imageUrl: "https://via.placeholder.com/150",
+      name: "Teclado Mecânico",
+      stock: 8,
+      price: 199.99,
+    },
     quantity: 3,
-    storeId: 103,
-    price: 199.99,
-    stock: 8,
-    name: "Teclado Mecânico"
+  
   },
   {
     id:4,
     productId: 4,
-    imageUrl: "https://via.placeholder.com/150",
     quantity: 1,
-    storeId: 104,
-    price: 599.0,
-    stock: 2,
-    name: "Monitor 24'' Full HD"
+    product:{
+      imageUrl: "https://via.placeholder.com/150",
+      price: 599.0,
+      stock: 2,
+      name: "Monitor 24'' Full HD"
+    }
+  
   },
   {
     id:5,
     productId: 5,
-    imageUrl: "https://via.placeholder.com/150",
+    product:{
+      imageUrl: "https://via.placeholder.com/150",
+      price: 29.5,
+      stock: 20,
+      name: "Cabo HDMI 2m"
+    },
     quantity: 4,
-    storeId: 105,
-    price: 29.5,
-    stock: 20,
-    name: "Cabo HDMI 2m"
+   
+  
   }
 ];
