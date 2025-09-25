20871c0b21403f29227cc75b3b8ed6c5a12d4447
aubert-co <blacket021@gmail.com>
2025-09-25 18:35:07 -0300

    chore(test): create unit tests folder

---
+++ app/backend/src/tests/unit/getMatchCategories.spec.ts
@@ -0,0 +1,8 @@
+import { getMatchCategories } from "../../helpers";
+
+describe("getMatchCategories",()=>{
+    it("test",()=>{
+        //expect(getMatchCategories('Esportes')).toEqual(["Beleza","Esporte","Roupas"])
+        expect(1).toEqual(1)
+    })
+})
