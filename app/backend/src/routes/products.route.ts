40c9a7ebb05cc91c91a9786f2456206401858365
aubert-co <blacket021@gmail.com>
2025-09-28 17:41:03 -0300

    refactor(product): remove Redis dependency injection from routes

--- app/backend/src/routes/products.route.ts
+++ app/backend/src/routes/products.route.ts
@@ -2,19 +2,15 @@ import { prisma } from "../lib/prisma";
 import { ProductRepository } from "../repository/product.repository";
 import { ProductService } from "../services/product.services";
 import { Router,Request,Response,NextFunction } from "express";
-import { ProductRedisRepository } from "../repository/redis.repository";
-import { ProductRedisService } from "../services/redis.services";
-import redis from "../lib/redis";
 import { ProductsController } from "../controller/products.controller";
 
 
 const productRepository = new ProductRepository(prisma)
-const productRedisRepository = new ProductRedisRepository(redis)
-const productRediService = new ProductRedisService( productRedisRepository)
 
-const productService = new ProductService(productRepository,productRediService)
 
-const productsController = new ProductsController(productService,productRediService)
+const productService = new ProductService(productRepository)
+
+const productsController = new ProductsController(productService)
 
 const route = Router()
 
