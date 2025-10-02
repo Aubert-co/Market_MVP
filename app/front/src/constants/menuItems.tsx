6c4b62dac46265605f2c035502b2dc435d1bc2c3
aubert-co <blacket021@gmail.com>
2025-10-02 17:49:19 -0300

    chore(menuBar): add back button to home

--- app/front/src/constants/menuItems.tsx
+++ app/front/src/constants/menuItems.tsx
@@ -1,11 +1,12 @@
 import type { SideBarItem } from "@/types/storeDashboard.types"
-import { FaBox, FaClipboardList, FaTags, FaUsers } from "react-icons/fa"
+import { FaBox, FaClipboardList, FaHome, FaTags, FaUsers } from "react-icons/fa"
 
 export const sideBarMenuItems:SideBarItem[] = [
   { label: "Dashboard", icon: <FaBox />, isActive: false, linkTo: "/loja" },
   { label: "Produtos", icon: <FaClipboardList />, isActive: true, linkTo: "/loja/produtos" },
   { label: "Cupons", icon: <FaTags />, isActive: false, linkTo: "/loja/cupons" },
   { label: "Pedidos", icon: <FaUsers />, isActive: false, linkTo: "/loja/pedidos" },
+  {label:"Inicio",icon:<FaHome/>,isActive:false,linkTo:"/"}
 ]
 
 type Label = "Dashboard" | "Produtos" | "Cupons" | "Pedidos" | "Configurações"
