403d81d245d4eb5e6a029658123f68a64aa30fd3
aubert-co <blacket021@gmail.com>
2025-10-09 15:57:19 -0300

    chore(store): update usable fetch function

--- app/front/src/pages/storeDashboard/storeCoupons.tsx
+++ app/front/src/pages/storeDashboard/storeCoupons.tsx
@@ -1,13 +1,13 @@
 import { FormCreateCoupon } from "@/components/forms/formCreateCoupon"
 import { ContainerDashboard } from "@/components/layouts/containerDashboard"
-import { usePagination } from "@/components/pagination"
+
 import { CouponTable } from "@/components/store/couponTable"
 import { selectMenuItem } from "@/constants/menuItems"
-import { getStoreCoupons } from "@/services/admStore.services"
-import { usableFetchWithPages } from "@/services/fetchs"
+import {  storeGetAvailableCoupons } from "@/services/admStore.services"
+import { usableFetch } from "@/services/fetchs"
 import { Box } from "@/styles/dashboardStore.style"
 import type { BaseCoupon } from "@/types/coupons.types"
-import type { GetStoreCoupons } from "@/types/storeDashboard.types"
+
 import { useEffect, useState } from "react"
 
 
@@ -21,24 +21,18 @@ export const StoreCoupons = ()=>{
     const [coupons,setCoupons] = useState<State>({
       datas:[],status:0,message:''
     })
-    const changePage = ()=>{};
-    const {Pagination,setPagesInfos,pageInfos} = usePagination(changePage)
-    
-    useEffect(()=>{
-      usableFetchWithPages<BaseCoupon<number>[],GetStoreCoupons>({
-        setDatas:setCoupons,
-        service:getStoreCoupons,
-        body:{nextPage:pageInfos.currentPage},
-        setPages:setPagesInfos
 
+    useEffect(()=>{
+      usableFetch<BaseCoupon<number>[],{}>({
+        setDatas:setCoupons,
+        service:storeGetAvailableCoupons,
+        body:{}
       })
     },[])
     return (
         <ContainerDashboard storeName="StoreName" sidebarMenuItems={selectMenuItem("Cupons")}>
           <Box>
-        
               <CouponTable coupons={coupons.datas}/>
-              <Pagination/>
           </Box>
            <FormCreateCoupon/>
         </ContainerDashboard>
