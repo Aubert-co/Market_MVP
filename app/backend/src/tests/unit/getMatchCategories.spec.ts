95bfdde3d013ec178f21f7b5d8ef0b448bd86ddb
aubert-co <blacket021@gmail.com>
2025-10-09 20:14:40 -0300

    chore: remove unused test

--- app/backend/src/tests/unit/getMatchCategories.spec.ts
+++
@@ -1,8 +0,0 @@
-import { getMatchCategories } from "../../helpers";
-
-describe("getMatchCategories",()=>{
-    it("test",()=>{
-        //expect(getMatchCategories('Esportes')).toEqual(["Beleza","Esporte","Roupas"])
-        expect(1).toEqual(1)
-    })
-})
