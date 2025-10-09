6a579a00f924831f250a537624c27940fb0760d7
aubert-co <blacket021@gmail.com>
2025-10-09 16:05:07 -0300

    chore(search): add service to search products

--- app/front/src/services/productsService.ts
+++ app/front/src/services/productsService.ts
@@ -1,5 +1,5 @@
 import type { Product } from "@/types/products.types";
-import type { ResponseWithPages} from "@/types/services.types"
+import type { ResponseDatas, ResponseWithPages} from "@/types/services.types"
 
 export type GetProductsIndex = {
     nextPage:string | number
@@ -12,12 +12,32 @@ export const serviceGetProducts = async({nextPage}:GetProductsIndex):Promise<Res
                 'Content-Type': 'application/json'
             }
         })
-        if(!response.ok){
-            return{ datas:[],message:'',status:response.status,totalPages:1,currentPage:1}
-        }
+        
         const {datas,currentPage,totalPages} = await response.json()
         return {datas,currentPage,totalPages,status:response.status,message:'sucesso'}
     }catch(err:unknown){
         return {datas:[] , currentPage:1,totalPages:1,status:500,message:'Algo deu errado!'}
+    }
+}
+export type BodySearch ={
+    name?:string,
+    category?:string,
+    minPrice?:string | number,
+    maxPrice?:string | number
+}
+export const searchProduct = async({name,category,minPrice,maxPrice}:BodySearch):Promise<ResponseDatas<Product[]> >=>{
+    try{
+     
+        const response = await fetch('/product/filter',{
+            method:'POST',
+            body:JSON.stringify({name,category,minPrice,maxPrice}),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        const {datas,message} = await response.json()
+        return {datas,message,status:response.status}
+    }catch(err:any){
+        return {datas:[],status:500,message:'Algo deu errado!'}
     }
 }
