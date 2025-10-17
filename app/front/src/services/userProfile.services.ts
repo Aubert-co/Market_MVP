daee02184142080a2be31eb3d01fa408dcbd75f8
aubert-co <blacket021@gmail.com>
2025-10-17 19:36:58 -0300

    chore(userProfile): add try/catch to userCoupons service

--- app/front/src/services/userProfile.services.ts
+++ app/front/src/services/userProfile.services.ts
@@ -6,19 +6,17 @@ import type { ResponseDatas } from "@/types/services.types"
 export const userCoupons = async():Promise<ResponseDatas<BaseCoupon<number>[]>>=>{
   
     try{
-        const response = await fetch('/user/list/coupons',{
-            method:'GET',
-            credentials:'include',
-            headers: {
-            'Content-Type': 'application/json'
-            }
-        })
-        if(!response.ok){
-            return {datas:[],message:'',status:response.status}
+      const response = await fetch('/user/list/coupons',{
+        method:'GET',
+        credentials:'include',
+        headers: {
+        'Content-Type': 'application/json'
         }
-        const {datas} = await response.json()
-        
-        return {datas,message:'Success',status:response.status}
+      })
+      
+      const {datas} = await response.json()
+      
+      return {datas,message:'Success',status:response.status}
         
     }catch(err:unknown){
         return {status:500,message:'Algo deu errado',datas:[] as BaseCoupon<number>[]}
