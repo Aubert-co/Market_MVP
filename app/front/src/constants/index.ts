280066fc7b7efa83ab593fc18154c385c58dc4c6
aubert-co <blacket021@gmail.com>
2025-10-17 19:34:04 -0300

    chore(checkout): add function to calculate the discount

--- app/front/src/constants/index.ts
+++ app/front/src/constants/index.ts
@@ -20,3 +20,4 @@ export const categories = [
 export const CART_KEY = "cart-item"
 export const FIVE_MINUTES = 5 * 60 * 1000;
 export const KEY_STORE = "store-key"
+export const CHECKOUT_KEY = "checkout-key"
