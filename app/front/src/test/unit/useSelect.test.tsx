5cfb75b261e93ae3d62df014836da7792f8f6c50
aubert-co <blacket021@gmail.com>
2025-11-21 23:37:46 -0300

    chore: rename hook from useSelectCategory to useSelect

---
+++ app/front/src/test/unit/useSelect.test.tsx
@@ -0,0 +1,25 @@
+import { useSelect } from "@/components/useSelect"
+import { fireEvent, render, } from "@testing-library/react"
+
+const mockDatas = [{value:'Eletrônicos',text:'Eletrônicos'},{value:'Todas',text:'Todas'}]
+describe("component useSelectCategory",()=>{
+    it("should update the select options correctly",()=>{
+        const selected = "Eletrônicos"
+        const MockComponent = ()=>{
+            const {Select} = useSelect({datas:mockDatas,text:'Selecione uma categoria'})
+        
+            return <Select/>
+        }
+        const {getByRole} = render(
+            <MockComponent/>
+        )
+        const select = getByRole('combobox');
+
+        fireEvent.change(select, { target: { value: selected } })
+        expect((select as HTMLSelectElement).value).toBe(selected);
+
+        fireEvent.change(select, { target: { value: 'Todas' } })
+
+        expect((select as HTMLSelectElement).value).toBe('Todas');
+    })
+})
