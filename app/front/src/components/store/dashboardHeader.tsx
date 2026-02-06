c7f6bc171d0751016b9435b0863c14fadb15549b
aubert-co <blacket021@gmail.com>
2026-02-05 23:36:36 -0300

    feat: add DashboardHeader

---
+++ app/front/src/components/store/dashboardHeader.tsx
@@ -0,0 +1,8 @@
+export const DashboardHeader = ()=>{
+    return (
+        <header>
+            <h1>Seu dashboard</h1>
+            <p>Acompanhe o desempenho da sua loja em tempo real.</p>
+        </header>
+    )
+}
