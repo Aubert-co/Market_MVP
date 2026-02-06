41044e1ebd3f73b5a798d23be62aab59ba4b126c
aubert-co <blacket021@gmail.com>
2026-02-05 23:20:03 -0300

    chore: add new values to sidebar menu items

--- app/front/src/constants/menuItems.tsx
+++ app/front/src/constants/menuItems.tsx
@@ -1,16 +1,15 @@
 import type { SideBarItem } from "@/types/storeDashboard.types"
-import { FaBox, FaClipboardList, FaHome, FaTags, FaUsers } from "react-icons/fa"
+import { FaBox, FaClipboardList, FaTags, FaUsers } from "react-icons/fa"
 
 export const sideBarMenuItems:SideBarItem[] = [
   { label: "Dashboard", icon: <FaBox />, isActive: false, linkTo: "/loja" },
   { label: "Produtos", icon: <FaClipboardList />, isActive: true, linkTo: "/loja/produtos" },
   { label: "Cupons", icon: <FaTags />, isActive: false, linkTo: "/loja/cupons" },
   { label: "Pedidos", icon: <FaUsers />, isActive: false, linkTo: "/loja/pedidos" },
-  {label:"Inicio",icon:<FaHome/>,isActive:false,linkTo:"/"}
 ]
 
-type Label = "Dashboard" | "Produtos" | "Cupons" | "Pedidos" | "Configurações"
-export const selectMenuItem = (label:Label)=>{
+export type SidebarLabel = "Dashboard" | "Produtos" | "Cupons" | "Pedidos" | "Configurações"
+export const selectMenuItem = (label:SidebarLabel)=>{
     return sideBarMenuItems.map((val)=>{
         val.isActive = false
         if(val.label === label){
