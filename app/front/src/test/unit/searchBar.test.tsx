b83fbd77409af40c0fd0667b0f6c0b4d77c55bc1
aubert-co <blacket021@gmail.com>
2026-02-28 23:58:01 -0300

    refactor(test): simplify SearchBar tests by removing hook tests

--- app/front/src/test/unit/searchBar.test.tsx
+++ app/front/src/test/unit/searchBar.test.tsx
@@ -1,14 +1,6 @@
-import { fireEvent, render, renderHook } from "@testing-library/react";
-import { SearchBar, useSearch } from "@/components/header/seachBar";
-import  { MemoryRouter} from "react-router-dom"
-import { act } from "react";
+import { fireEvent, render } from "@testing-library/react";
+import { SearchBar } from "@/components/header/seachBar";
 
-const mockedUsedNavigate = jest.fn()
-
-jest.mock("react-router-dom", () => ({
-  ...jest.requireActual("react-router-dom"),
-  useNavigate: () => mockedUsedNavigate,
-}))
 const searchEvent = jest.fn()
 describe("component SearchBar",()=>{
     beforeEach(()=>{
@@ -42,42 +34,3 @@ describe("component SearchBar",()=>{
     })
 })
 
-
-describe("hook useSearch",()=>{
-    beforeEach(()=>{
-        jest.clearAllMocks()
-    })
-   it("should navigate when mode is navigate", () => {
-    const value = "notebook"
-    const { result } = renderHook(
-      () => useSearch({ mode: "navigate" }),
-      {
-        wrapper: MemoryRouter
-      }
-    )
-
-    act(() => {
-      result.current.searchEvent( value  )
-    })
-
-    expect(mockedUsedNavigate).toHaveBeenCalledWith({
-      pathname: "/buscas",
-      search: `q=${value}`
-    });
-  })
-
-  it("should update searchProduct when mode is data", () => {
-    const { result } = renderHook(
-      () => useSearch({ mode: "update" }),
-      {
-        wrapper: MemoryRouter
-      }
-    )
-
-    act(() => {
-      result.current.searchEvent("mouse")
-    })
-
-    expect(result.current.searchProduct).toBe("mouse")
-  })
-})
