95dfb34388363d22ac2a32f34312abbba98f8113
aubert-co <blacket021@gmail.com>
2026-03-03 23:11:48 -0300

    feat(useSelect): add initialValue and cbSelected support

--- app/front/src/hooks/useSelect.tsx
+++ app/front/src/hooks/useSelect.tsx
@@ -1,28 +1,43 @@
 import {   useState } from "react"
-import type { DatasSelect } from "@/types/filters";
+import type { DatasSelect } from "@/types/filters.types";
 
 
 export type Props<T extends string | number> = {
     datas: DatasSelect<T>[];
     text?: string;
-    className?:string
+    className?:string,
+    name:string,
+    initialValue ?:T
+    cbSelected?:(value:T)=>void
 };
-export const useSelect = <T extends string,>({datas,text,className}:Props<T>)=>{
-    const [selected,setSelected] = useState<T >(datas[0].value)
+export const useSelect = <T extends string,>({datas,text,className,name,initialValue,cbSelected}:Props<T>)=>{
+    const [selected,setSelected] = useState<T >(initialValue ?? datas[0].value)
    
+    const onChange = (value:T)=>{
+        setSelected(value)
+        cbSelected?.(value)
+     
+    }
     const Select = ()=>{
         return (
-        <select className={className} value={selected ?? ""}
-         onChange={(e)=>setSelected(e.target.value as T)}>
-            <option 
-            value={""}
-             disabled >{text}</option>
-            {datas.map((val,index)=>
+       <select
+            name={name}
+            className={className}
+            value={selected ?? ""}
+            onChange={(e) => onChange(e.target.value as T)}
+            aria-label={text}
+            >
+            <option value="" disabled>
+                {text}
+            </option>
+
+            {datas.map((val, index) => (
                 <option value={val.value} key={index}>
-                    {val.text}
+                {val.text}
                 </option>
-            )}
-        </select>)
+            ))}
+        </select>
+        )
     }
     return {selected,setSelected,Select}
 }
