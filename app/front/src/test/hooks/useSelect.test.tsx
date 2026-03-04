3a133a4e2cd478d0d076144d507b7e61a32dda69
aubert-co <blacket021@gmail.com>
2026-03-03 23:13:28 -0300

    test(useSelect): add more tests and replace fireEvent with userEvent

--- app/front/src/test/hooks/useSelect.test.tsx
+++ app/front/src/test/hooks/useSelect.test.tsx
@@ -1,12 +1,16 @@
 import { useSelect } from "@/hooks/useSelect"
-import { fireEvent, render, } from "@testing-library/react"
+import type { Category } from "@/types/filters.types"
+import {  render, renderHook, } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
 
 const mockDatas = [{value:'Eletrônicos',text:'Eletrônicos'},{value:'Todas',text:'Todas'}]
 describe("component useSelectCategory",()=>{
-    it("should update the select options correctly",()=>{
+    it("should update the select options correctly",async()=>{
         const selected = "Eletrônicos"
+        const cbSelected  = jest.fn()
         const MockComponent = ()=>{
-            const {Select} = useSelect({datas:mockDatas,text:'Selecione uma categoria',name:"input-select"})
+            const {Select} = useSelect({datas:mockDatas,text:'Selecione uma categoria',
+                name:"input-select",cbSelected})
         
             return <Select/>
         }
@@ -14,12 +18,27 @@ describe("component useSelectCategory",()=>{
             <MockComponent/>
         )
         const select = getByRole('combobox');
+        const user = userEvent.setup()
+        await user.selectOptions(select, selected);
 
-        fireEvent.change(select, { target: { value: selected } })
         expect((select as HTMLSelectElement).value).toBe(selected);
-
-        fireEvent.change(select, { target: { value: 'Todas' } })
-
+        expect( cbSelected ).toHaveBeenCalledTimes(1)
+        expect(cbSelected).toHaveBeenCalledWith(selected)
+        await user.selectOptions(select,'Todas')
+        expect(cbSelected).toHaveBeenCalledWith('Todas')
         expect((select as HTMLSelectElement).value).toBe('Todas');
+       
     })
+    it("should initialize with first option when no initialValue", () => {
+        const { result } = renderHook(() =>
+                useSelect<Category>({
+                datas:mockDatas,
+                text: "Selecione categoria",
+                name: "category",
+                initialValue:"Roupas"
+                })
+        );
+
+        expect(result.current.selected).toBe("Roupas");
+    });
 })
