5c7a603f17ade4cb5e94283898e95442a45e8aa4
aubert-co <blacket021@gmail.com>
2026-03-05 20:41:11 -0300

    refactor: update return structure in getStorageStore

--- app/front/src/storage/store.storage.ts
+++ app/front/src/storage/store.storage.ts
@@ -3,15 +3,17 @@ import type { Store } from "@/types/store.types";
 
 
 
-export const getStorageStore  =():Store[]=>{
+export const getStorageStore  =():Store=>{
     const localStore = localStorage.getItem(KEY_STORE)
     if(localStore){
         return JSON.parse( localStore ) 
     }
-    return [] as Store[];
+    return {
+        id:0,name:"",photo:"",description:""
+    } ;
 }
 
 
-export const saveStorageStore = (store:Store[]):void=>{
+export const saveStorageStore = (store:Store):void=>{
     localStorage.setItem( KEY_STORE , JSON.stringify( store ) )
 }
