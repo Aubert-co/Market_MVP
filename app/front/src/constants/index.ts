bbafe8a12384a2b07edd11573a9443b7c68523b5
aubert-co <blacket021@gmail.com>
2026-03-06 23:54:40 -0300

    refactor(constants): rename some constants for better clarity

--- app/front/src/constants/index.ts
+++ app/front/src/constants/index.ts
@@ -1,4 +1,5 @@
-import type { CategoryMapped ,DatasSelect} from "@/types/filters";
+import type { CategoryOption ,DatasSelect, ProductSortOption, SelectOrderStatus} from "@/types/filters.types";
+
 
 export const categories = [
   "Roupas",
@@ -18,12 +19,30 @@ export const categories = [
   "Acessórios"
 ];
 
+export const allCategoriesOption = ['Todas',...categories] satisfies CategoryOption[]
 
-export const mappedCateogires = ['Todas',...categories].map((val)=>{
+export const categorySelectOptions = allCategoriesOption.map((val)=>{
   return { text:val,value:val}
-}) as DatasSelect<CategoryMapped>[]
+}) as DatasSelect<CategoryOption>[]
 
 export const CART_KEY = "cart-item"
 export const FIVE_MINUTES = 5 * 60 * 1000;
 export const KEY_STORE = "store-key"
 export const CHECKOUT_KEY = "checkout-key"
+
+export const PRODUCT_SORT_ARRAY = [
+  "price_asc","price_desc","stock_asc","stock_desc"
+] satisfies ProductSortOption[];
+
+export const PRODUCT_SORT_OPTIONS: DatasSelect<ProductSortOption>[] = [
+  { value: "price_asc", text: "Menor preço" },
+  { value: "price_desc", text: "Maior preço" },
+  { value: "stock_asc", text: "Menor estoque" },
+  { value: "stock_desc", text: "Maior estoque" },
+];
+
+export const ORDER_STATUS_OPTIONS:SelectOrderStatus[] = [
+  {text:'completo',value:'completed'},
+  {text:'pendente',value:'pending'},
+  {text:'cancelado',value:'cancelled'}
+] 
