399d0987657cb430bb3a28c5f38472094cc5fcb1
aubert-co <blacket021@gmail.com>
2026-03-09 23:22:52 -0300

    feat(store): add store routes and ProtectedStoreRoutes

--- app/front/src/pages/router.tsx
+++ app/front/src/pages/router.tsx
@@ -1,19 +1,19 @@
 import { BrowserRouter as Router ,Routes,Route} from "react-router-dom"
 import { Register } from "./register"
-
 import { Login } from "./login"
 import { Index } from "."
 import { ProductDetail } from "./productDetail"
 import { Profile } from "./profile"
-import { CreateStore } from "./storeDashboard/createStore"
-import { StoreAdminDash } from "./storeDashboard/storeDashboard"
-import { StoreProducts } from "./storeDashboard/storeProducts"
-import { StoreCoupons } from "./storeDashboard/storeCoupons"
-import { StoreOrders } from "./storeDashboard/storeOrders"
+import { CreateStore } from "./store/createStore"
+import { StoreHome } from "./store/storeHome"
+import { StoreProducts } from "./store/storeProducts"
+import { StoreCoupons } from "./store/storeCoupons"
+import { StoreOrders } from "./store/storeOrders"
 import { Coupon } from "./coupon"
 import { NotFound } from "./not_found"
 import { Search } from "./search"
 import { Checkout } from "./checkout"
+import { ProtectedStoreRoutes } from "@/components/store/protectedStoreRoute"
 export const App = ()=>{
     return(
     <Router>
@@ -26,14 +26,18 @@ export const App = ()=>{
                 <Route path="/perfil/:action" element={<Profile/>}/>
                 <Route path="/produto/:productid" element={<ProductDetail/>}/>
                 <Route path="/abrir-loja" element={<CreateStore/>}/>
-                <Route path="/loja" element={<StoreAdminDash/>}/>
-                <Route path="/loja/produtos" element={<StoreProducts/>}/>
-                <Route path="/loja/cupons" element={<StoreCoupons/>}/>
-                <Route path="/loja/pedidos" element={<StoreOrders/>}/>
                 <Route path="/cupons" element={<Coupon/>} />
                 <Route path="*" element={<NotFound/>}/>
-                <Route path="/buscas/:product" element={<Search/>}/>
+                <Route path="/buscas/*" element={<Search/>}/>
                 <Route path="/pagamento" element={<Checkout/>}/>
+                
+                <Route element={<ProtectedStoreRoutes/>}>
+                    <Route path="/loja" element={<StoreHome/>}/>
+                    <Route path="/loja/produtos" element={<StoreProducts/>}/>
+                    <Route path="/loja/cupons" element={<StoreCoupons/>}/>
+                    <Route path="/loja/pedidos" element={<StoreOrders/>}/>
+                </Route>
+           
             </Route>
         </Routes>
     </Router>
