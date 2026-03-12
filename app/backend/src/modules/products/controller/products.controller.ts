9881b07b94368209335ab475a91bf2d1fb8f316e
aubert-co <blacket021@gmail.com>
2026-03-11 22:48:27 -0300

    refactor: reorganize products module structure

---
+++ app/backend/src/modules/products/controller/products.controller.ts
@@ -0,0 +1,83 @@
+import { NextFunction, Request, Response } from "express";
+import { IProductService } from "../services/product.services";
+import { checkIsAValidCategory, checkIsAValidNumber, isAValidString } from "../../../helpers";
+import { ErrorMessage } from "../../../helpers/ErrorMessage";
+import { checkOrderBy } from "../../../helpers";
+
+export class ProductsController{
+    constructor(protected products:IProductService){}
+    public async GetProducts(req:Request,res:Response,next:NextFunction):Promise<any>{
+        let page = Number(req.params?.page)
+       
+        if( !checkIsAValidNumber(page) ){
+            page = 1
+        }
+        try{
+            const {datas,currentPage,fromCache,totalPages} = await this.products.getProducts(page)
+        
+            res.status(200).send({message:'Sucess',datas,totalPages,currentPage,fromCache})
+            
+        }catch(err:any){
+            next(err)
+        }
+    } 
+    public async GetOneProduct(req:Request,res:Response,next:NextFunction):Promise<any>{
+        const id = req.params?.id
+        if(!checkIsAValidNumber(id)){
+            return res.status(500).send({message:"Failed to retrieve products. Please try again later."});
+        }
+        try{
+            const values = await this.products.getProductById(Number(id));
+            
+            const datas = {
+                product: values.product ? [values.product] : [],
+                ratings: values.ratings,
+                comments: values.product?.comments ?? [],
+                reviews: values.product?.reviews ?? []
+            };
+
+            res.status(200).send({ message: 'Success', datas });
+        }catch(err:any){
+            next(err)
+        }
+            
+    }
+    public async filterProducts(req:Request,res:Response,next:NextFunction):Promise<any>{
+        try{
+            const name = req.body?.name
+            let category = req.body?.category
+            const minPrice = req.body?.minPrice
+            const maxPrice = req.body?.maxPrice
+            let orderBy = req.body?.orderBy ?? 'desc'
+            const take = 10
+            const skip =0
+            
+            if(!checkOrderBy(orderBy))orderBy = "desc"
+            if(category && category.toLowerCase() ===  "todas")category = "";
+            if(category && !checkIsAValidCategory(category)) {
+                throw new ErrorMessage("Invalid category provided", 400);
+            }
+
+            if (name && !isAValidString(name)) {
+                throw new ErrorMessage("Invalid name format", 400);
+            }
+
+            if (maxPrice && !checkIsAValidNumber(maxPrice)) {
+                throw new ErrorMessage("Invalid maximum price value", 400);
+            }
+
+            if (minPrice && !checkIsAValidNumber(minPrice)) {
+                throw new ErrorMessage("Invalid minimum price value", 400);
+            }
+             
+            const datas = await this.products.filterProduct({
+                name,category,maxPrice,minPrice,take,
+                skip,orderBy
+            })
+       
+            res.status(200).send({message:'Sucess',datas})
+        }catch(err:any){
+            next(err)
+        }
+    }
+}
---
+++ app/backend/src/modules/products/routes/productAdmin.route.ts
@@ -0,0 +1,32 @@
+import { prisma } from "../../../lib/prisma";
+import { ProductAdminController } from "../controller/productAdmin.controller";
+import { Router,Request,Response,NextFunction } from "express"
+import { fileUpload } from "../../../lib/fileUpload";
+import { Auth } from "../../../middleware/auth";
+import { VerifyStoreOwnership } from "../../../middleware/verifyStoreOwnership";
+
+import { StoreRepository } from "../../store/repository/store.repository";
+import { ProductAdminRepository } from "../repository/productAdmin.repository";
+import { StoreService } from "../../store/services/store.services";
+import { ProductAdminService } from "../services/productAdmin.services";
+
+const storeRepository = new StoreRepository(prisma)
+
+const productAdminRepository = new ProductAdminRepository(prisma)
+const productAdminService = new ProductAdminService(productAdminRepository)
+const storeService = new StoreService(storeRepository)
+
+const verifyStoreOwnershipMiddle = new VerifyStoreOwnership(storeService)
+const productAdmin = new ProductAdminController(productAdminService)
+const route = Router()
+
+
+route.post('/product/create',[
+    fileUpload.single('image')
+    ,Auth
+    ,(req:Request,res:Response,next:NextFunction)=>verifyStoreOwnershipMiddle.handler(req,res,next)
+],
+  (req:Request,res:Response,next:NextFunction)=> productAdmin.createProduct(req,res,next)
+)
+
+export default route
