6012f9d8a783f21bd538c00bfd9bcb4214a2d299
aubert-co <blacket021@gmail.com>
2026-03-14 23:37:24 -0300

    refactor(storage): convert googleStorage into a class responsible for Google image storage

--- app/backend/src/lib/googleStorage.ts
+++ app/backend/src/lib/googleStorage.ts
@@ -1,63 +1,44 @@
-import  {Storage}  from '@google-cloud/storage';
-import { Buffer } from 'buffer'; 
+import  {Bucket, Storage}  from '@google-cloud/storage';
 import { ErrorMessage } from '../helpers/ErrorMessage';
-import sharp from 'sharp';
+import { IFileStorage } from './ImageUploadService';
+import { ImageStorage, UploadFile, UploadImageResult } from 'types/storageImages.types';
 
-const PRIVATE_KEY = process.env.PRIVATE_KEY
-if(!PRIVATE_KEY)throw new Error("no private key");
- 
-const KEY_STORAGE = JSON.parse( PRIVATE_KEY )
-const storage = new Storage({
-    credentials:KEY_STORAGE,
-    projectId:KEY_STORAGE.project_id
-})
-const BUCKET_NAME = process.env.BUCKET_NAME ;
 
-if (!BUCKET_NAME ) {
-    throw new Error("no bucket name");
-} 
-const bucket = storage.bucket(BUCKET_NAME)
 
-async function compressImage(fileBuffer: Buffer): Promise<Buffer | null> {
-  try {
-    const MAX_WIDTH_LARGE = 1200;
-    const MAX_WIDTH_MEDIUM = 800;
-    const MAX_WIDTH_SMALL = 600;
+export class GoogleStorage implements IFileStorage , ImageStorage{
+  private readonly PRIVATE_KEY: string
+  private readonly BUCKET_NAME: string
 
-    const fileSizeKB = fileBuffer.byteLength / 1024;
+  private storage: Storage
+  private bucket: Bucket
 
-    if (fileSizeKB < 100) {
-      
-      return fileBuffer;
+  constructor() {
+
+    const privateKey = process.env.PRIVATE_KEY
+    const bucketName = process.env.BUCKET_NAME
+
+    if (!privateKey) {
+      throw new Error("Missing PRIVATE_KEY")
     }
 
-    let width = MAX_WIDTH_MEDIUM;
-    let quality = 80;
-
-    if (fileSizeKB > 500) {
-      width = MAX_WIDTH_SMALL;
-      quality = 70;
+    if (!bucketName) {
+      throw new Error("Missing BUCKET_NAME")
     }
 
-    const optimizedBuffer = await sharp(fileBuffer)
-      .resize({ width, withoutEnlargement: true })
-      .jpeg({ quality })
-      .toBuffer();
+    this.PRIVATE_KEY = privateKey
+    this.BUCKET_NAME = bucketName
 
-    return optimizedBuffer;
-  } catch (error:any) {
-    return null;
+    const keyStorage = JSON.parse(this.PRIVATE_KEY)
+
+    this.storage = new Storage({
+      credentials: keyStorage,
+      projectId: keyStorage.project_id
+    })
+
+    this.bucket = this.storage.bucket(this.BUCKET_NAME)
   }
-}
-
-type UploadFile ={
-  fileBuffer:Buffer,
-  urlPath:string,
-  mimeType:string
-}
-export const uploadFileToGCS = async ({fileBuffer,mimeType,urlPath}:UploadFile): Promise<string> => {
-    const buff = await compressImage(fileBuffer)
-    const blob = bucket.file( urlPath );
+   async upload({ fileBuffer, urlPath, mimeType }:UploadFile): Promise<UploadImageResult> {
+    const blob = this.bucket.file( urlPath );
     const blobStream = blob.createWriteStream({
         resumable: false,
         contentType:mimeType,
@@ -65,46 +46,51 @@ export const uploadFileToGCS = async ({fileBuffer,mimeType,urlPath}:UploadFile):
         cacheControl: 'public, max-age=31536000',         },
     });
 
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
         blobStream.on('error', err => {
-        reject(err);
+          resolve({
+            success: false,
+            error: "upload-error"
+          })
         });
 
         blobStream.on('finish', () => {
-          resolve('sucess')
+          resolve({success:true})
         });
-        if(buff){
-          blobStream.end(buff);
+        if(fileBuffer){
+          blobStream.end(fileBuffer);
           return 
         }
         blobStream.end(fileBuffer)
     });
-    
-};
-export const generateSignedUrl = async (imageName: string) => {
-  try {
-    const options = {
-      version: "v4" as const,
-      action: "read" as const,
-      expires: Date.now() + 15 * 60 * 1000, 
-    };
-
-    const [url] = await storage
-      .bucket(BUCKET_NAME)
-      .file(imageName)
-      .getSignedUrl(options);
-
-    return url;
-  } catch (error) {
-   
-    return "";
   }
-};
+  async generateSignedUrl(imageName: string): Promise<string> {
+  try {
+      const options = {
+        version: "v4" as const,
+        action: "read" as const,
+        expires: Date.now() + 15 * 60 * 1000, 
+      };
 
-export const deleteImgFromBucket = async(imageName:string)=>{
-  try{
-    await storage.bucket(BUCKET_NAME).file(imageName).delete()
-  }catch(err:any){
-    throw new ErrorMessage("Failed to delete image",500)
+      const [url] = await this.storage
+        .bucket(this.BUCKET_NAME)
+        .file(imageName)
+        .getSignedUrl(options);
+
+        return url;
+      } catch (error) {
+    
+        return "";  
+    }
+  }
+  async deleteImg(imageName: string): Promise<any> {
+    try{
+      await this.storage.bucket(this.BUCKET_NAME).file(imageName).delete()
+    }catch(err:any){
+      throw new ErrorMessage("Failed to delete image",500)
+    }
   }
 }
+
+
+
