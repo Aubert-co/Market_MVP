d01f2e971ba5cbff83df34be51b41211109beb3f
aubert-co <blacket021@gmail.com>
2026-03-15 23:29:29 -0300

    feat: add generic factory module

---
+++ app/backend/src/factory/uploadFIles.ts
@@ -0,0 +1,7 @@
+import { ImageUploadService } from "services/ImageUploadService";
+import { AwsStorage } from "../lib/awsStorage";
+
+export function makeUploadFile(){
+    const storage = new AwsStorage()
+    return new ImageUploadService(storage)
+}
