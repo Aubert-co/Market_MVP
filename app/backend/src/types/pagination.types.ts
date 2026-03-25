b1f490a39f7e7825badf6cc5a5d501ad9ea7db86
aubert-co <blacket021@gmail.com>
2026-03-24 22:26:11 -0300

    feat(types): add pagination types

---
+++ app/backend/src/types/pagination.types.ts
@@ -0,0 +1,10 @@
+export type Pagination = {
+    totalPages:number,
+    skip:number,
+    currentPage:number
+}
+export type PaginationDTO={
+    skip:number,
+    page:number,
+    limit:number
+}
