b9bf95a1a5446427482627703a739a7fd9939dbe
aubert-co <blacket021@gmail.com>
2026-03-27 22:57:23 -0300

    feat(factory): add redis to makeVerifyStoreMiddle

--- app/backend/src/factory/makeVerifyStoreMiddle.ts
+++ app/backend/src/factory/makeVerifyStoreMiddle.ts
@@ -2,10 +2,15 @@ import { StoreService } from "../modules/store/services/store.services";
 import { prisma } from "../lib/prisma";
 import { StoreRepository } from "../modules/store/repository/store.repository";
 import { VerifyStoreOwnership } from "../middleware/verifyStoreOwnership";
+import { ProductRedisRepository } from "../repository/redis.repository";
+import redis from "../lib/redis";
+import { ProductRedisService } from "../services/redis.services";
 
 
 export function makeVerifyStoreMiddle(){
     const storeRepository = new StoreRepository(prisma)
     const storeService = new StoreService(storeRepository)
-    return new VerifyStoreOwnership(storeService)
+    const redisRepository = new ProductRedisRepository(redis)
+    const redisService = new ProductRedisService(redisRepository)
+    return new VerifyStoreOwnership(storeService , redisService)
 } 
