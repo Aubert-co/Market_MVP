3aec2c56f2d2483013a741fa0a357110129d1a0e
aubert-co <blacket021@gmail.com>
2026-03-27 22:59:18 -0300

    feat(redis): add store cache logic

--- app/backend/src/services/redis.services.ts
+++ app/backend/src/services/redis.services.ts
@@ -1,11 +1,14 @@
 import { IProductRedisRepository } from "../repository/redis.repository";
 import { Product,SelectedProduct } from "../modules/products/types/product.types";
+import { CacheStoreId } from "types/cache.types";
 
 export interface IProductRedisService{
     getCountProductInCache():Promise<number>,
     saveProductInCache(key:string,data:SelectedProduct[]):Promise<any>,
     saveCountProductsInCache(countProducts:number):Promise<void>,
-    getProductInCache(key:string):Promise<SelectedProduct[] >
+    getProductInCache(key:string):Promise<SelectedProduct[] >,
+    getcachedStoreId(key:string):Promise<CacheStoreId | null>,
+    saveItemsCache<T>(key:string,datas:T):Promise<void>
 }
 
 export class ProductRedisService implements IProductRedisService{
@@ -48,6 +51,31 @@ export class ProductRedisService implements IProductRedisService{
             return 0;
         }catch(err:any){
             return 0;
+        }
+    }
+    protected async getCacheByKey<T>(key:string):Promise<T | null>{
+        try{
+            const cachedDatas = await this.redis.getCachedProduct(key)
+        
+            if(!cachedDatas)return null ;
+            
+            const parsed = JSON.parse(cachedDatas)
+            
+            return parsed as T
+        }catch{
+            return null;
+        }
+        
+    }
+    public async saveItemsCache<T>(key:string,datas:T){
+        const parsedDatas = JSON.stringify(datas)
+        await this.redis.saveProductInCache(key,parsedDatas)
+    }
+    public async getcachedStoreId(key:string):Promise<CacheStoreId | null>{
+        try{
+            return await this.getCacheByKey<CacheStoreId>(key)
+        }catch{
+            return null;
         }
     }
 }
