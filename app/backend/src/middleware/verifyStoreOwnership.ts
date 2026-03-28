5fc655ca82257f84c67d533964e411a517a1ad42
aubert-co <blacket021@gmail.com>
2026-03-27 22:58:12 -0300

    feat(middleware): add cache for store ownership verification

--- app/backend/src/middleware/verifyStoreOwnership.ts
+++ app/backend/src/middleware/verifyStoreOwnership.ts
@@ -1,44 +1,53 @@
 import { NextFunction ,Request,Response} from "express";
 import { checkIsAValidNumber } from "../helpers";
 import { IStoreService } from "../modules/store/services/store.services";
+import { IProductRedisService } from "services/redis.services";
 
 
 
 
 export class VerifyStoreOwnership{
-    constructor(protected store:IStoreService){}
-    public getStoreId(params: any, body: any): number {
-        const idFromBody = body?.storeId ?? body;
-        const idFromParams = params?.storeId ?? params;
+    constructor(protected store:IStoreService , protected redis:IProductRedisService){}
+    protected getStoreId(req:Request): number {
+        const bodyStoreId = req.body.storeId
+        const paramsStoreId = req.params.storeId
+        const queryStoreId = req.query.storeId
 
-        if (checkIsAValidNumber(idFromBody)) {
-            return Number(idFromBody);
+        if(bodyStoreId && checkIsAValidNumber(bodyStoreId)){
+            return Number(bodyStoreId)
         }
-
-        if (checkIsAValidNumber(idFromParams)) {
-            return Number(idFromParams);
+        if(paramsStoreId && checkIsAValidNumber(paramsStoreId)){
+            return Number(paramsStoreId)
         }
-
-        return 0;
+        if(queryStoreId && checkIsAValidNumber(queryStoreId)){
+            return Number(queryStoreId)
+        }
+        return 0
     }
     public async handler(req:Request,res:Response,next:NextFunction):Promise<any>{
        
        
         const userId = req.user
 
-        const storeId = this.getStoreId(req.params,req.body)
+        const storeId = this.getStoreId(req)
         if(storeId === 0){ 
             return res.status(400).send({message:'Invalid store ID.'})
         }
+        const key = `user:${userId}:store:${storeId}`
+      
         try{
+            const getCachedItems = await this.redis.getcachedStoreId(key)
+         
+            if(getCachedItems)return next();
             
-            const check = await this.store.checkOwnerShip(Number(storeId),Number(userId))
+            const check = await this.store.checkOwnerShip(storeId,Number(userId))
            
             if(!check) {
                 return res.status(403).send({ message: "You do not have permission to access this store." });
             }
+            await this.redis.saveItemsCache(key,{userId,storeId})   
             next()
-        } catch (err: any) {
+        } catch(err:unknown) {
            
             return res.status(500).send({ message: "An unexpected error occurred." });
         }
