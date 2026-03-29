4a31051ebd5e503233a0758ac84f899bb8c6f98f
aubert-co <blacket021@gmail.com>
2026-03-29 18:49:38 -0300

    refactor: move validation checkers to checkIsValid.ts

--- app/backend/src/helpers/index.ts
+++ app/backend/src/helpers/index.ts
@@ -1,39 +1,3 @@
-import { StatusOrder } from "../modules/orders/types/order.types";
-
-export const isAValidString =(value:any,maxLength:number = 15):boolean=>{
-    if(!value || typeof value !== 'string' )return false;
-
-    if(value.length <= 4 || value.length >= maxLength)return false;
-    return true;
-}
-
-export const isValidEmail = (email:any):boolean=>{
-
-   const emailRegex = new RegExp(
-    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-  );
-  return emailRegex.test(email);
-}
-
-export const checkIsAValidNumber = (value:any):boolean=>{
-   if (typeof value === 'boolean') return false;
-
-  const number = Number(value);
-
-  if (
-    value === null ||
-    value === undefined ||
-    value === '' ||
-    isNaN(number) ||
-    number <= 0
-  ) {
-    return false;
-  }
-  const isValidFormat = /^[0-9]+(\.[0-9]+)?$/.test(String(value)); 
-  if(!isValidFormat)return false;
-  return true;
-}
-
 export const categories = [
   "Roupas",
   "Eletrônicos",
@@ -51,54 +15,14 @@ export const categories = [
   "Moda",
   "Acessórios"
 ];
-const orderBy = ["asc","desc"]
-export const checkOrderBy = (value:unknown):boolean=>{
-  if(!value || typeof value !=='string')return false
-  return orderBy.includes(value.toLowerCase())
-}
-export function checkIsValidStatus(value: unknown): value is StatusOrder {
-  return typeof value === "string" && ["PENDING","PAID","CANCELED"].includes(value)
-}
-const normalizeString = (str: string) =>
-  str.normalize("NFD")             
-     .replace(/[\u0300-\u036f]/g, "") 
-     .toLowerCase();
-     
-export const checkIsAValidCategory = (category:string)=>{
- const normalizedInput = normalizeString(category);
+export const orderBy = ["asc","desc"]
 
-  return categories.some(cat => normalizeString(cat) === normalizedInput);
-}
-
-
-type Pagination = {
-  totalItems:number,
-  page:number,
-  limit:number
-}
-type ReturnPagination = {
-  currentPage:number,
-  totalPages:number,
-  skip:number
-}
-export const pagination = ({totalItems,page,limit}:Pagination):ReturnPagination=>{
-  const totalPages = Math.ceil(totalItems/limit)
-  if(page > totalPages){
-    page = totalPages
-  }
-  const skip = (page -1)* limit
-  return{
-    currentPage:page,
-    skip,
-    totalPages
-  }
-}
-
+ 
 export const roundTottaly = (value:number, decimals:number = 2):number=> {
   return Math.round(value * Math.pow(10, decimals)) / Math.pow(10, decimals);
 }
 
-export const calcSkipPages = (page:number,limit:number):number=>(page-1)*limit
+
 
 export  const getString = (value: unknown) =>
 typeof value === "string" ? value : undefined;
