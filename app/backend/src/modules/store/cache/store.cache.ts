7b5b9bc006485887fa98f5d6559e5402b37d46be
aubert-co <blacket021@gmail.com>
2026-03-30 19:50:08 -0300

    feat: implement store cache structure

---
+++ app/backend/src/modules/store/cache/store.cache.ts
@@ -0,0 +1,32 @@
+import { BaseRedisServices } from "../../../services/BaseRedis.service";
+
+export interface IStoreCache {
+    saveCacheStoreId(storeId:number,userId:number):Promise<void>
+    getCachedStoreId(userId:number,storeId:number):Promise<number | null>
+}
+const KEY_STOREID =(userId:number,storeId:number)=> `user:${userId}:store:${storeId}`
+
+export class StoreCache extends BaseRedisServices implements IStoreCache{
+    async saveCacheStoreId(storeId:number,userId:number):Promise<void>{
+       try{
+        const key = KEY_STOREID(userId,storeId)
+        const expirationTime = 3600;
+        await this.saveItemsInCache<number>({
+            key,
+            data:storeId,
+            expirationTime
+        })
+       }catch{
+        return 
+       }
+    }
+
+    async getCachedStoreId(userId:number,storeId:number):Promise<number | null>{
+        try{
+            const key = KEY_STOREID(userId,storeId)
+            return await this.getCacheByKey<number>(key)
+        }catch{
+            return null
+        }
+    }
+}
