f61c1bcf1cdf83f909048043714de22b1369ffd1
aubert-co <blacket021@gmail.com>
2026-04-07 23:55:15 -0300

    feat: implement pino-loki integration in logger

--- app/backend/src/lib/logger.ts
+++ app/backend/src/lib/logger.ts
@@ -1,5 +1,47 @@
 import pino from 'pino'
 
-export const logger = pino({
-  level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
-})
+export const startLogger = () => {
+  const env = process.env.NODE_ENV ?? 'development'
+
+
+  if (env === 'test') {
+    return pino({ level: 'debug' })
+  }
+
+  const {
+    LOKI_URL,
+    LOKI_USERNAME,
+    LOKI_PASSWORD
+  } = process.env
+
+  if (!LOKI_URL || !LOKI_USERNAME || !LOKI_PASSWORD) {
+    throw new Error('Missing Loki environment variables')
+  }
+
+  const transport = pino.transport({
+    target: 'pino-loki',
+    options: {
+      host: LOKI_URL,
+      basicAuth: {
+        username: LOKI_USERNAME,
+        password: LOKI_PASSWORD
+      },
+      labels: {
+        service: 'api',
+        env
+      },
+      batching: true
+    }
+  })
+
+  return pino(
+    {
+      level: env === 'production' ? 'info' : 'debug',
+      base: {
+        service: 'api'
+      },
+      timestamp: pino.stdTimeFunctions.isoTime
+    },
+    transport
+  )
+}
