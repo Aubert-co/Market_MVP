b22ef39423ca4a3879eaecb4e99434204815bda0
aubert-co <blacket021@gmail.com>
2026-04-09 23:31:54 -0300

    feat(tests/fixtures): add users fixtures

---
+++ app/backend/src/tests/__fixtures__/users.ts
@@ -0,0 +1,5 @@
+export const users = [
+  { name: 'lucas', password: '1234456', email: 'joses@gmail.com', id: 1 },
+  { name: 'marcos', password: 'abcdef', email: 'marcos@gmail.com', id: 2 },
+  { name: 'ana', password: 'qwerty', email: 'ana@gmail.com', id: 3 }
+]
