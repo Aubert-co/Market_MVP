5a36c5eda0df119c7678537d2be596b0df49460b
aubert-co <blacket021@gmail.com>
2026-04-10 23:48:33 -0300

    feat: rename users fixture to manyUsers for clarity

--- app/backend/src/tests/__fixtures__/users.ts
+++ app/backend/src/tests/__fixtures__/users.ts
@@ -1,4 +1,4 @@
-export const users = [
+export const manyUsers = [
   { name: 'lucas', password: '1234456', email: 'joses@gmail.com', id: 1 },
   { name: 'marcos', password: 'abcdef', email: 'marcos@gmail.com', id: 2 },
   { name: 'ana', password: 'qwerty', email: 'ana@gmail.com', id: 3 }
