57202925a82c1fb6b4ca300ab37f87c5be5f132c
aubert-co <blacket021@gmail.com>
2026-04-15 22:51:22 -0300

    fix(admin-orders): fix lastOrders route and add store verification middleware

--- app/backend/src/modules/storeAdmin/orders/orders.routes.ts
+++ app/backend/src/modules/storeAdmin/orders/orders.routes.ts
@@ -21,7 +21,7 @@ route.get('/stores/:storeId/orders',
 )
 
 route.get('/stores/:storeId/lastOrders',
-
+    [(req:Request,res:Response,next:NextFunction)=>makeVerifyStoreMiddle().handler(req,res,next)],
     (req:Request,res:Response,next:NextFunction)=>ordersController.getLastOrders(req,res,next)
 )
 export default route
