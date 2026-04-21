168b655eb8584d223680c8f9d724fb38821869a5
aubert-co <blacket021@gmail.com>
2026-04-20 23:41:19 -0300

    feat(products): add session middleware to products routes

--- app/backend/src/modules/products/routes/products.route.ts
+++ app/backend/src/modules/products/routes/products.route.ts
@@ -6,15 +6,20 @@ import { ProductsController } from "../controller/products.controller";
 import { CacheProducts } from "../cache/product.cache";
 import { RedisRepository } from "../../../repository/redis.repository";
 import redis from "../../../lib/redis";
+import { ViewsRepository } from "@/modules/views/repository/views.repository";
+import { ViewsServices } from "@/modules/views/services/views.services";
+import { userSessionMiddleware } from "@/middleware/userSession.middeware";
 
 const productRepository = new ProductRepository(prisma)
 const redisRepository = new RedisRepository(redis)
 const cacheProducts = new CacheProducts(redisRepository)
 const productService = new ProductService(productRepository,cacheProducts)
-
-const productsController = new ProductsController(productService)
+const viewRepository = new ViewsRepository(prisma)
+const viewService = new ViewsServices(viewRepository)
+const productsController = new ProductsController(productService,viewService)
 
 const route = Router()
+
 
 route.post('/product/filter',
     (req:Request,res:Response,next:NextFunction)=>productsController.filterProducts(req,res,next)
@@ -23,6 +28,7 @@ route.get('/product/page/:page',
     (req:Request,res:Response,next:NextFunction)=> productsController.GetProducts(req,res,next)
 );
 route.get('/product/:id',
+    [(req:Request,res:Response,next:NextFunction)=>userSessionMiddleware(req,res,next)],
     (req:Request,res:Response,next:NextFunction)=> productsController.GetOneProduct(req,res,next)
 )
  
