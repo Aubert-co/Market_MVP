105f944ad3d399dd2e87c53efdae5aa760f68aef
aubert-co <blacket021@gmail.com>
2026-04-25 16:49:36 -0300

    refactor(upload): remove file compression logic from ImageUploadService

--- app/backend/src/services/ImageUploadService.ts
+++ app/backend/src/services/ImageUploadService.ts
@@ -1,4 +1,3 @@
-import { compressImage } from "@/helpers/compressImages";
 import { UploadFile, UploadImageResult,IFileStorage } from "@/types/storageImages.types";
 
 export interface IImageUploadService{
@@ -10,11 +9,9 @@ export class ImageUploadService implements IImageUploadService {
     constructor(private storage:IFileStorage){}
 
     public async uploadImage({fileBuffer,urlPath,mimeType}:UploadFile):Promise<UploadImageResult>{
-      const buff = await compressImage(fileBuffer)
-      if(!buff) return {success:false,error:'compress-error'};
-
+     
       return await this.storage.upload({
-        fileBuffer:buff,urlPath,mimeType
+        fileBuffer,urlPath,mimeType
       })
     }
    public async generateSignedUrl(imageName:string):Promise<string>{
