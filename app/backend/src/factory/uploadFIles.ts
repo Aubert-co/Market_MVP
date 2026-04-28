10bbebe15bfb6fe3e5153805b79a946f12faafb5
aubert-co <blacket021@gmail.com>
2026-04-27 22:08:01 -0300

    feat: add upload provider to enable mocking in tests

--- app/backend/src/factory/uploadFIles.ts
+++ app/backend/src/factory/uploadFIles.ts
@@ -1,9 +1,13 @@
 import { IImageUploadService, ImageUploadService } from "@/services/ImageUploadService";
 import { AwsStorage } from "@/lib/awsStorage";
 //import { GoogleStorage } from "@/lib/googleStorage";
-
+import { ImageUploadProvier } from "@/provider/providerImageStorage";
 
 export function makeUploadFile():IImageUploadService{
+
+    if(process.env.NODE_ENV === "test-e2e"){
+        return new ImageUploadProvier()
+    }
     const storage = new AwsStorage()
     return new ImageUploadService(storage)
 }
