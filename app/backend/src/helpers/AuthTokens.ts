905b3f89b324979ef740f33887e1e8887f2e2f11
aubert-co <blacket021@gmail.com>
2026-05-03 23:27:05 -0300

    feat: add cookieConfig to AuthTokens helper

--- app/backend/src/helpers/AuthTokens.ts
+++ app/backend/src/helpers/AuthTokens.ts
@@ -1,9 +1,9 @@
 import jwt from 'jsonwebtoken'
-import { ErrorMessage } from './ErrorMessage';
+import { CookieOptions } from 'express';
 
 const ACCESS_TOKEN = process.env?.ACCESS_TOKEN;
 const REFRESH_TOKEN = process.env?.REFRESH_TOKEN;
-
+const isProduction = process.env.NODE_ENV === "production"
 if(!ACCESS_TOKEN || !REFRESH_TOKEN){
   throw new Error("Falha no .env")
 }
@@ -16,3 +16,24 @@ export const generateRefreshToken = (userId: number) => {
  
   return jwt.sign({ id: userId }, REFRESH_TOKEN, { expiresIn: '7d' });
 };
+
+
+
+export const cookieConfig = (): CookieOptions => {
+  const baseConfig: CookieOptions = {
+    httpOnly: true,
+    secure: isProduction,
+    maxAge: 15 * 60 * 1000,
+    path: '/',
+    sameSite: isProduction ? 'none' : 'lax'
+  }
+
+  if (isProduction) {
+    return {
+      ...baseConfig,
+      domain: '.aubertdev.com.br'
+    }
+  }
+
+  return baseConfig
+}
