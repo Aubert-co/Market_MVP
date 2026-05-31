0c7866b479f80ef7a2ba1732ec732228c3a616c2
aubert-co <blacket021@gmail.com>
2026-05-30 23:51:55 -0300

    test(fixtures): add cart items fixture

---
+++ app/backend/src/tests/__fixtures__/cart.ts
@@ -0,0 +1,25 @@
+export const manyCartItems = [
+ 
+  { id: 1, userId: 1, productId: 2, quantity: 2 }, 
+  { id: 2, userId: 1, productId: 3, quantity: 1 }, 
+  { id: 3, userId: 1, productId: 5, quantity: 3 }, 
+  { id: 4, userId: 1, productId: 6, quantity: 1 }, 
+
+
+  { id: 5, userId: 2, productId: 1, quantity: 1 }, 
+  { id: 6, userId: 2, productId: 3, quantity: 2 }, 
+  { id: 7, userId: 2, productId: 7, quantity: 1 }, 
+  { id: 8, userId: 2, productId: 9, quantity: 4 }, 
+
+
+  { id: 9, userId: 3, productId: 1, quantity: 2 }, 
+  { id: 10, userId: 3, productId: 2, quantity: 1 }, 
+  { id: 11, userId: 3, productId: 4, quantity: 1 }, 
+  { id: 12, userId: 3, productId: 5, quantity: 2 }, 
+
+
+  { id: 13, userId: 1, productId: 8, quantity: 1 }, 
+  { id: 14, userId: 2, productId: 10, quantity: 2 }, 
+  { id: 15, userId: 3, productId: 14, quantity: 1 }, 
+  { id: 16, userId: 1, productId: 15, quantity: 3 }, 
+]
