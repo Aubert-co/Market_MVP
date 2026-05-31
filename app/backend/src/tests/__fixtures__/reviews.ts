3bd78c8dacb3859705994a1d817610cd53cab879
aubert-co <blacket021@gmail.com>
2026-05-30 23:52:29 -0300

    test(fixtures): add reviews fixture

---
+++ app/backend/src/tests/__fixtures__/reviews.ts
@@ -0,0 +1,58 @@
+export const manyReviews = [
+  {
+    id: 1,
+    userId: 3,
+    productId: 1,
+    orderId: 1,
+    rating: 5,
+    createdAt: new Date("2025-09-02T10:00:00Z")
+  },
+  {
+    id: 2,
+    userId: 3,
+    productId: 4,
+    orderId: 3,
+    rating: 4,
+    createdAt: new Date("2025-09-11T09:00:00Z")
+  },
+  {
+    id: 3,
+    userId: 3,
+    productId: 8,
+    orderId: 11,
+    rating: 5,
+    createdAt: new Date("2025-09-28T11:00:00Z")
+  },
+  {
+    id: 4,
+    userId: 3,
+    productId: 11,
+    orderId: 13,
+    rating: 4,
+    createdAt: new Date("2025-09-30T13:00:00Z")
+  },
+  {
+    id: 5,
+    userId: 3,
+    productId: 16,
+    orderId: 16,
+    rating: 5,
+    createdAt: new Date("2025-10-03T08:00:00Z")
+  },
+  {
+    id: 6,
+    userId: 3,
+    productId: 18,
+    orderId: 18,
+    rating: 3,
+    createdAt: new Date("2025-10-05T14:00:00Z")
+  },
+  {
+    id: 7,
+    userId: 3,
+    productId: 2,
+    orderId: 20,
+    rating: 4,
+    createdAt: new Date("2025-10-07T10:00:00Z")
+  }
+]
