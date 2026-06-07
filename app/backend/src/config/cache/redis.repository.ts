2ecd3881e20caa499a1ec30f05a231c31c83661d
aubert-co <blacket021@gmail.com>
2026-06-06 23:01:15 -0300

    feat: add increment method to Redis repository

--- app/backend/src/config/cache/redis.repository.ts
+++ app/backend/src/config/cache/redis.repository.ts
@@ -3,6 +3,8 @@ import { RedisClientType } from "redis";
 export interface IRedisRepository{
     saveItemInCache(key:string,datas:string,expirationTime:number):Promise<void>
     getCachedItem(key:string):Promise<string | null>,
+    deleteCacheKey(key:string):Promise<void>,
+    redisIncrement(key:string):Promise<void>
 }
 
 export class RedisRepository implements IRedisRepository{
@@ -22,5 +24,10 @@ export class RedisRepository implements IRedisRepository{
     public async getCachedItem(key:string):Promise<string | null>{
         return await this.redis.get(key)
     }
-   
+    public async deleteCacheKey(key:string):Promise<void>{
+        await this.redis.del(key)
+    }
+    public async redisIncrement(key:string):Promise<void>{
+        await this.redis.incr(key)
+    }
 }
